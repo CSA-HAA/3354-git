25969cdb751a2624f528fdb0da588fbd36a56b1b
Hamzah Ahmed <hahmed970@gmail.com>
2020-09-23 23:43:08 -0500

    adder

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     private static int addArguments(String[] args) {
         int total = 0
-        for(int i=index;i<args.length;i++){
+        for(int i=0;i<args.length;i++){
             total+=Integer.valueOf(args[i]) ;
         }
         return total;
